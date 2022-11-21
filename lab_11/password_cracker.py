221a18383350cb3c6d5e544098aa7a39a484f230
Valkor Simpson <hns13@students.uwf.edu>
2022-11-21 10:24:16 -0600

    finished part 2

--- lab_11/password_cracker.py
+++ lab_11/password_cracker.py
@@ -4,8 +4,10 @@ import hashlib
 ## prompt the user to input the hashed password to crack
 
 ## -----------
-hash_type = input("hash type: ")
-password_file = input("password file: ")
+#password = qwerty (MD5):  d8578edf8458ce06fbc5bb76a58c5ca4
+#password = qwerty (SHA1): b1b3773a05c0ed0176787a4f1574ff0075f7521e
+hash_type = input("hash type: ") 
+password_file = input("password file (please include .txt)): ")
 hashed_password = input("hashed password to crack: ")
 ## --------------
 
@@ -22,5 +24,22 @@ hashed_password = input("hashed password to crack: ")
     ## print('Password not in file') if none of the password matches
     
 ## -----------
-## Your code goes here
+file = open(password_file, 'r')
+found_password = False
+for line in file:
+    hash
+    if hash_type == "md5": 
+        hash = hashlib.md5(line.strip().encode()).hexdigest()
+    elif hash_type == "sha1": 
+        hash = hashlib.sha1(line.strip().encode()).hexdigest()
+
+    if hash == hashed_password:
+        print("password found: " + line.strip())
+        found_password = True
+        break
+    
+file.close()
+if not found_password:
+    print('Password not in file')
+exit
 ## --------------
